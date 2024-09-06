02b49f09e92aa5f2c7be1cec6ef7319d0791b910
BuildTools <simon.thibaud@epitech.eu>
2024-09-06 10:36:20 +0200

    {+}[BACKEND] New route create-user for using the db + Test token and refresh token with Postman

--- test_technique_db/runserver/views.py
+++ test_technique_db/runserver/views.py
@@ -1,7 +1,19 @@
 from rest_framework import viewsets
 from .models import Article
 from .serializers import ArticleSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer
 
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+
+class CreateUserView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- test_technique_db/test_technique_db/urls.py
+++ test_technique_db/test_technique_db/urls.py
@@ -7,6 +7,7 @@ from rest_framework_simplejwt.views import (
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from runserver.views import CreateUserView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -24,4 +25,5 @@ urlpatterns = [
     path('api/', include('runserver.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/create-user/', CreateUserView.as_view(), name='create_user'),
 ]
