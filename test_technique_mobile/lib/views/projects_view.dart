3ecda584e2771b69e460b5f2c2a0ab6416c3c97a
SimonTHD <simon.thibaud@epitech.eu>
2024-09-07 18:55:12 +0200

    {+}[Mobile] Added two buttons add project and delete project linked with db

--- test_technique_mobile/lib/views/projects_view.dart
+++ test_technique_mobile/lib/views/projects_view.dart
@@ -48,10 +48,100 @@ class _ProjectsViewState extends State<ProjectsView> {
     }
   }
 
+  Future<void> _addProject(String githubLink, String description) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('access_token');
+    print("token addproject: $token");
+    if (token != null) {
+      final response = await http.post(
+        Uri.parse('http://192.168.1.39:8000/projects/'),
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/json',
+        },
+        body: json.encode({'github_link': githubLink, 'description': description}),
+      );
+      print('Status code: ${response.statusCode}');
+      print('Response body: ${response.body}');
+      if (response.statusCode == 201) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Projet ajouté avec succès')),
+        );
+        _fetchProjects();
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Erreur lors de l\'ajout du projet')),
+        );
+      }
+    }
+  }
+  Future<void> _deleteProject(int projectId) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('access_token');
+    if (token != null) {
+      final response = await http.delete(
+        Uri.parse('http://192.168.1.39:8000/projects/delete/$projectId/'),
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      );
+      if (response.statusCode == 204) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Projet supprimé avec succès')),
+        );
+        _fetchProjects();
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Erreur lors de la suppression du projet')),
+        );
+      }
+    }
+  }
+  void _showAddProjectDialog() {
+    final TextEditingController nameController = TextEditingController();
+    final TextEditingController descriptionController = TextEditingController();
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Ajouter un projet'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(labelText: 'Nom du projet'),
+              ),
+              TextField(
+                controller: descriptionController,
+                decoration: const InputDecoration(labelText: 'Description'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: const Text('Annuler'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            ElevatedButton(
+              child: const Text('Ajouter'),
+              onPressed: () {
+                _addProject(nameController.text, descriptionController.text);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
    @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Projets de l\'utilisateur')),
+      appBar: AppBar(title: const Text('Projets de l\'utilisateur')),
       body: _projects.isNotEmpty
           ? Padding(
               padding: const EdgeInsets.all(8.0),
@@ -65,19 +155,27 @@ class _ProjectsViewState extends State<ProjectsView> {
                 itemCount: _projects.length,
                 itemBuilder: (context, index) {
                   final project = _projects[index];
-                  return ProjectCard(project: project);
+                  return ProjectCard(
+                    project: project,
+                    onDelete: () => _deleteProject(project['id']),
+                  );
                 },
               ),
             )
           : const Center(child: CircularProgressIndicator()),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showAddProjectDialog,
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
 
 class ProjectCard extends StatelessWidget {
   final Map<String, dynamic> project;
+  final VoidCallback onDelete;
 
-  const ProjectCard({super.key, required this.project});
+  const ProjectCard({super.key, required this.project, required this.onDelete});
 
   @override
   Widget build(BuildContext context) {
@@ -116,6 +214,13 @@ class ProjectCard extends StatelessWidget {
               project['description'] ?? 'Aucune description',
               maxLines: 3,
               overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: IconButton(
+              icon: const Icon(Icons.delete, color: Colors.red),
+              onPressed: onDelete,
             ),
           ),
         ],
